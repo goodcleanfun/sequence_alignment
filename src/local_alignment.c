736067dd2339575e6c7e31f364761f4471db6017
Al <albarrentine@gmail.com>
2023-12-28 02:05:55 -0500

    [alignment/local] reducing the space by a factor of 4 by removing the conversion to unicode codepoints, instead operating on the raw string and iterating over the string using utf8proc. The function now expects strings encoded in UTF-8 and the lengths to be specified in terms of unicode codepoints. In fact there is now nothing proportional to n (the larger string) that needs to be allocated. All internal arrays are the size of m, the smaller string, which works especially well for aligning a small query sequence to a large one.

--- src/local_alignment.c
+++ src/local_alignment.c
@@ -1,6 +1,6 @@
 #include "local_alignment.h"
 #include "utf8/utf8.h"
-
+#include "utf8proc/utf8proc.h"
 
 static alignment_ops_t NULL_ALIGNMENT_OPS = {
     .num_matches = 0,
@@ -15,22 +15,19 @@ static inline bool utf8_is_non_alphanumeric(int32_t c) {
     return utf8_is_whitespace(c) || utf8_is_hyphen(c) || utf8_is_punctuation(cat);
 }
 
-alignment_ops_t affine_gap_align_op_counts_unicode_options(uint32_array *u1_array, uint32_array *u2_array, alignment_options_t options) {
-    if (u1_array->n < u2_array->n) {
-        uint32_array *tmp_array = u1_array;
-        u1_array = u2_array;
-        u2_array = tmp_array;
+alignment_ops_t affine_gap_align_op_counts_options(const char *s1, size_t m, const char *s2, size_t n, alignment_options_t options) {
+    if (m < n) {
+        const char *tmp_str = s1;
+        s1 = s2;
+        s2 = tmp_str;
+        size_t tmp_len = m;
+        m = n;
+        n = tmp_len;
     }
-
-    size_t m = u1_array->n;
-    size_t n = u2_array->n;
-
-    uint32_t *u1 = u1_array->a;
-    uint32_t *u2 = u2_array->a;
 
     alignment_ops_t result = NULL_ALIGNMENT_OPS;
 
-    if (unicode_equals(u1_array, u2_array)) {
+    if (m == n && utf8_compare_len(s1, s2, n) == 0) {
         result.num_matches = n;
         return result;
     }
@@ -123,10 +120,20 @@ alignment_ops_t affine_gap_align_op_counts_unicode_options(uint32_array *u1_arra
     bool in_gap = false;
     size_t prev_c = 0;
 
+    uint8_t *s2_ptr = (uint8_t *)s2;
+    int32_t c1 = 0;
+    int32_t c1_prev = 0;
+    int32_t c2 = 0;
+    int32_t c2_prev = 0;
+
     for (size_t i = 1; i < n + 1; i++) {
         // s = CC[0]
         s = costs[0];
-        uint32_t c2 = u2[i - 1];
+
+        c2 = 0;
+        ssize_t c2_len = utf8proc_iterate(s2_ptr, -1, &c2);
+        if (c2 == 0) break;
+
         // CC[0] = c = t = t + h
         t += gap_extend_cost;
         c = t;
@@ -145,12 +152,16 @@ alignment_ops_t affine_gap_align_op_counts_unicode_options(uint32_array *u1_arra
         alignment_op op = ALIGN_GAP_OPEN;
 
         size_t min_cost = max_cost;
+        uint8_t *s1_ptr = (uint8_t *)s1;
 
         for (size_t j = 1; j < m + 1; j++) {
             // insertion
             // e = min(e, c + g) + h
             size_t min = e;
-            uint32_t c1 = u1[j - 1];
+
+            c1 = 0;
+            ssize_t c1_len = utf8proc_iterate(s1_ptr, -1, &c1);
+            if (c1 == 0) break;
 
             current_edits = edits[j];
 
@@ -219,7 +230,7 @@ alignment_ops_t affine_gap_align_op_counts_unicode_options(uint32_array *u1_arra
                 }
             }
 
-            if (j > 1 && i > 1 && c1 == u2[i - 2] && c2 == u1[j - 2] && c1 != u1[j - 2]) {
+            if (j > 1 && i > 1 && c1 == c2_prev && c2 == c1_prev && c1 != c1_prev) {
                 if (prev_row_prev2_cost + transpose_cost < min) {
                     s = prev_row_prev2_cost;
                     w = transpose_cost;
@@ -261,7 +272,12 @@ alignment_ops_t affine_gap_align_op_counts_unicode_options(uint32_array *u1_arra
             prev_char_edits = current_edits;
             edits[j] = current_edits;
 
+            s1_ptr += c1_len;
+            c1_prev = c1;
         }
+
+        s2_ptr += c2_len;
+        c2_prev = c2;
     }
 
     result = edits[m];
@@ -276,33 +292,11 @@ alignment_ops_t affine_gap_align_op_counts_unicode_options(uint32_array *u1_arra
 
 }
 
-
-alignment_ops_t affine_gap_align_op_counts_unicode(uint32_array *u1_array, uint32_array *u2_array) {
-    return affine_gap_align_op_counts_unicode_options(u1_array, u2_array, DEFAULT_ALIGNMENT_OPTIONS_AFFINE_GAP);
-}
-
-alignment_ops_t affine_gap_align_op_counts_options(const char *s1, const char *s2, alignment_options_t options) {
-    if (s1 == NULL || s2 == NULL) return NULL_ALIGNMENT_OPS;
-
-    uint32_array *u1_array = unicode_codepoints(s1);
-    if (u1_array == NULL) return NULL_ALIGNMENT_OPS;
-
-    uint32_array *u2_array = unicode_codepoints(s2);
-
-    if (u2_array == NULL) {
-        uint32_array_destroy(u1_array);
-        return NULL_ALIGNMENT_OPS;
-    }
-
-    alignment_ops_t edits = affine_gap_align_op_counts_unicode_options(u1_array, u2_array, options);
-
-    uint32_array_destroy(u1_array);
-    uint32_array_destroy(u2_array);
-
-    return edits;
-}
-
-
 alignment_ops_t affine_gap_align_op_counts(const char *s1, const char *s2) {
-    return affine_gap_align_op_counts_options(s1, s2, DEFAULT_ALIGNMENT_OPTIONS_AFFINE_GAP);
+    if (s1 == NULL || s2 == NULL) return NULL_ALIGNMENT_OPS;
+    size_t s1_len = strlen(s1);
+    size_t m = utf8_len(s1, s1_len);
+    size_t s2_len = strlen(s2);
+    size_t n = utf8_len(s2, s2_len);
+    return affine_gap_align_op_counts_options(s1, m, s2, n, DEFAULT_ALIGNMENT_OPTIONS_AFFINE_GAP);
 }
--- src/local_alignment.h
+++ src/local_alignment.h
@@ -5,11 +5,8 @@
 #include <stdlib.h>
 
 #include "alignment.h"
-#include "num_arrays/uint32_array.h"
 
 alignment_ops_t affine_gap_align_op_counts(const char *s1, const char *s2);
-alignment_ops_t affine_gap_align_op_counts_options(const char *s1, const char *s2, alignment_options_t options);
-alignment_ops_t affine_gap_align_op_counts_unicode(uint32_array *u1_array, uint32_array *u2_array);
-alignment_ops_t affine_gap_align_op_counts_unicode_options(uint32_array *u1_array, uint32_array *u2_array, alignment_options_t options);
+alignment_ops_t affine_gap_align_op_counts_options(const char *s1, size_t m, const char *s2, size_t n, alignment_options_t options);
 
 #endif
